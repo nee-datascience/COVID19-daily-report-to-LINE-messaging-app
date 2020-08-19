d32c94e012bfa7e99955e5ebe9626cd763d60b87
Nee Datascience <65205267+nee-datascience@users.noreply.github.com>
2020-08-19 11:27:07 +1000

    Update covid.py
    Updated indention

--- covid.py
+++ covid.py
@@ -3,30 +3,40 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as soup
 
-# Scraping
+# Define scraping function with country as input 
 def Scraping(Country):
  try:
- 	driver = webdriver.Edge('c:\\Python37\\msedgedriver.exe')
- 	url = 'https://www.worldometers.info/coronavirus/country/'+Country
-
- 	driver.get(url)
- 	page_html = driver.page_source
- 	data = soup(page_html, 'html.parser')
+  # Specify browser driver path e.g. using MS Edge in this case. 
+ 	#driver = webdriver.Edge('c:\\Python37\\msedgedriver.exe')
+    driver = webdriver.Chrome('D:\\python\\chromedriver_win32\\chromedriver.exe')
+  
+  # Specify URL to scrape
+    url = 'https://www.worldometers.info/coronavirus/country/'+Country
+     
+  # Get data in html format
+    driver.get(url)
+    page_html = driver.page_source
+  # Beautify data from html source
+    data = soup(page_html, 'html.parser')
  	
- 	new_date = data.findAll('div',{'class':'news_date'})
- 	new_case = data.findAll('li',{'class':'news_li'})
- 	new_cases = new_case[0].text.replace('[source]','')
- 	total = new_date[0].text + ': ' + new_cases
- 	driver.close()
- 	return total
+  # Get number of new cases 
+    new_date = data.findAll('div',{'class':'news_date'})
+    new_case = data.findAll('li',{'class':'news_li'})
+    new_cases = new_case[0].text.replace('[source]','')
+    total = new_date[0].text + ': ' + new_cases
+
+  # Close website
+    driver.close()
+    return total
  except:
  	return 'No Result'
 
 # Send message to LINE messaging app 
 from songline import Sendline
-token = 'cxTFICPuXUU9mgbjiJBYqgzZnxxs1jdNDNuzqHVktux'	#Web Scraping
-#token = 'oOCTcbTSfucGOKrdbTTdEb1wWYn8EhIwOJuveH1zCDI'	#COVID-19 Report
+token = '{Enter your LINE token here}' #COVID BOT Group in LINE App
 messenger = Sendline(token)
 
 message = Scraping('australia')
+messenger.sendtext(message)
+message = Scraping('new-zealand')
 messenger.sendtext(message)
