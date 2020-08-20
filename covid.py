203750ff37d4fe799dfb1fac7ca44f24099243df
Nee Datascience <65205267+nee-datascience@users.noreply.github.com>
2020-08-20 19:15:52 +1000

    Update covid.py
    Remove driver code and  update sending message

--- covid.py
+++ covid.py
@@ -7,17 +7,11 @@ from bs4 import BeautifulSoup as soup
 # Define scraping function with country as input 
 def Scraping(Country):
  try:
-  # Specify browser driver path e.g. using MS Edge in this case. 
- 	#driver = webdriver.Edge('c:\\Python37\\msedgedriver.exe')
-  #driver = webdriver.Chrome('D:\\python\\chromedriver_win32\\chromedriver.exe')
-  
+
   # Specify URL to scrape
     url = 'https://www.worldometers.info/coronavirus/country/'+Country
      
   # Get data in html format
-    #Replace driver with html parser
-    #driver.get(url)
-    #page_html = driver.page_source
     page_html = requests.get(url).text
     
   # Beautify data from html source
@@ -30,8 +24,8 @@ def Scraping(Country):
     total = new_date[0].text + ': ' + new_cases
 
   # Close website
-    #driver.close()
     return total
+  
  except:
  	return 'No Result'
 
@@ -40,7 +34,5 @@ from songline import Sendline
 token = '{Enter your LINE token here}' #COVID BOT Group in LINE App
 messenger = Sendline(token)
 
-message = Scraping('australia')
-messenger.sendtext(message)
-message = Scraping('new-zealand')
-messenger.sendtext(message)
+messenger.sendtext(Scraping('australia'))
+messenger.sendtext(Scraping('new-zealand'))
