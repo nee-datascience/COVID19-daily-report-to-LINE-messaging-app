b22e28613e7d86d30e20474fa11be81c8a81e854
Nee Datascience <65205267+nee-datascience@users.noreply.github.com>
2020-08-20 19:08:51 +1000

    Update covid.py
    Replaced driver with html parser.

--- covid.py
+++ covid.py
@@ -1,4 +1,5 @@
 # Import required packages
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as soup
@@ -8,14 +9,17 @@ def Scraping(Country):
  try:
   # Specify browser driver path e.g. using MS Edge in this case. 
  	#driver = webdriver.Edge('c:\\Python37\\msedgedriver.exe')
-    driver = webdriver.Chrome('D:\\python\\chromedriver_win32\\chromedriver.exe')
+  #driver = webdriver.Chrome('D:\\python\\chromedriver_win32\\chromedriver.exe')
   
   # Specify URL to scrape
     url = 'https://www.worldometers.info/coronavirus/country/'+Country
      
   # Get data in html format
-    driver.get(url)
-    page_html = driver.page_source
+    #Replace driver with html parser
+    #driver.get(url)
+    #page_html = driver.page_source
+    page_html = requests.get(url).text
+    
   # Beautify data from html source
     data = soup(page_html, 'html.parser')
  	
@@ -26,7 +30,7 @@ def Scraping(Country):
     total = new_date[0].text + ': ' + new_cases
 
   # Close website
-    driver.close()
+    #driver.close()
     return total
  except:
  	return 'No Result'
